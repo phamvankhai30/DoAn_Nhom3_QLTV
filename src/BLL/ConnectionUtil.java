56cfc94ab7b03e8fa8b6f4f8ae5bde9edf9b9e4f
khai <khai@192.168.78.1>
2020-04-17 15:42:42 +0700

    Đồ án QLTV

---
+++ src/BLL/ConnectionUtil.java
@@ -0,0 +1,21 @@
+package BLL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    static String hostName = "localhost";//mặc định
+
+	static String dbName = "LoginDB";//tên database
+	static String Username = "root";//mặc định
+	static String Password = "";//mặc định
+	
+	
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		 String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+		 
+	     Connection conn = DriverManager.getConnection(connectionURL, Username, Password);
+	     return conn;
+	}
+}
